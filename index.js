15e8c977d103278470a707807662fce6bb75167c
nathanieladiah <nathanieladiah@gmail.com>
2023-06-19 08:42:46 -0400

    Configure morgan so it shows data sent in POST requests

--- index.js
+++ index.js
@@ -3,8 +3,34 @@ var morgan = require("morgan");
 
 const app = express();
 
+// Create tokens for stfff
+// console.log(response.req.body);
+// console.log(request.body);
+
+morgan.token("postData", (req, res) => JSON.stringify(req.body));
+
+// Create a function that logs the data if it's post
+const loggingFunction = (tokens, req, res) => {
+  let message = [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, "content-length"),
+    "-",
+    tokens["response-time"](req, res),
+    "ms",
+  ].join(" ");
+  // use Post data only if it's POST method
+  if (req.method === "POST") {
+    return `${message}  ${tokens.postData(req, res)}`;
+  } else {
+    return message;
+  }
+};
+
 app.use(express.json());
-app.use(morgan("tiny"));
+// app.use(morgan("tiny"));
+app.use(morgan(loggingFunction));
 
 let phonebook = [
   {
@@ -91,7 +117,6 @@ app.post("/api/persons", (request, response) => {
   };
 
   phonebook = phonebook.concat(person);
-
   response.json(person);
 });
 
